030e060a54941c67581ad80c1c95c771f791c2d6
unknown <jcuetos97@gmail.com>
2022-11-15 19:56:48 -0600

    Created: header component in header.js
    Header component includes About, Explore, Login and Sign Up links, as well as logo and name of project.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Explore from './pages/Explore';
+
+// Construct our main GraphQL API endpoint
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Header/>
+            <div>
+              <Routes>
+                <Route
+                path='/'
+                element={<Home />}  
+                />
+                <Route
+                path='/about'
+                element={<About />}  
+                />
+                <Route
+                path='/explore'
+                element={<Explore />}  
+                />
+              </Routes>
+            </div>
+          <Footer/>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Logo from '../assets/png/logo.png';
+import { Link } from 'react-router-dom';
+
+import About from '../pages/About';
+import Explore from '../pages/Explore';
+
+// CSS Style
+import '../assets/css/header.css';
+import '../assets/css/general.css';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='header-content'>
+                <div className='header-logo-container'>
+                    <div className='header-logo-img-cont'>
+                        <img src={Logo} alt="Company Logo" className="header-logo-img"/>
+                    </div>
+                    <p className='header-logo-title'>man<br/>da<br/>la</p>     
+                </div>
+                <div className='header-main'>
+                    <ul className='header-links'>
+                        <li className='header-link-wrapper'>
+                            <Link to={About}>
+                            <span className='header-link'>About</span>
+                            </Link>
+                        </li>
+                        <li className='header-link-wrapper'>
+                            <Link to={Explore}>
+                            <span className='header-link'>Explore</span>
+                            </Link>
+                        </li>
+                        <li className='header-link-wrapper'>
+                            <Link to={About}>
+                            <span className='header-link'>Login</span>
+                            </Link>
+                        </li>
+                        <li className='header-link-wrapper'>
+                            <Link to={About}>
+                            <span className='header-link'>Sign Up</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+
+export default Header;
+
+
