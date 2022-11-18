60a5d88544b0a46e4cddb1a25d04a8a04f24bd13
qgtere <86640621+qgtere@users.noreply.github.com>
2022-11-17 18:12:57 -0700

    Add models, schemas and server functionality

---
+++ server/models/Item.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const itemSchema = new Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },  
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const Item = model('Item', itemSchema);
+
+module.exports = Item;
---
+++ server/models/User.js
@@ -0,0 +1,84 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    street: {
+      type: String,
+    },  
+    zip: {
+      type: Number,
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    boughtItems: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Item'
+        }
+    ],
+    saleItems: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Item'
+        }
+    ],
+    soldItems: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: 'Item'
+      }
+    ],    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    }
+  }
+);
+
+// hash user password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+// when we query a user, we'll also get another field called `soldItems` with the number of items already sold
+userSchema.virtual('soldCount').get(function () {
+  return this.soldItems.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
