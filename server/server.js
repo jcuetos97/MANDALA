921763911b2ed35bd040aeeb19be917467e9ffb6
qgtere <86640621+qgtere@users.noreply.github.com>
2022-11-28 08:25:58 -0700

    save new item info into database ("sell" functionality) and save the image on the server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const cors = require('cors');
 const { authMiddleware } = require('./utils/auth');
-const cors = require('cors')
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
@@ -16,6 +15,10 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+app.use(cors());
+//routes
+app.use('/', require('./routes/uploadImage'));
+
 
 // This is the checkout session
 require('dotenv').config();
@@ -25,7 +28,7 @@ app.use(express.static('public'));
 const YOUR_DOMAIN = 'http://localhost:3000';
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.post('/create-checkout-session', async (req, res) => {
   
   let line_items = req.body.price_ID.map((item) => {
