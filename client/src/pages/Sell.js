729424308cd209725f0a9a253ead32c99133b260
cjclap <dmxtechinn@gmail.com>
2022-11-27 19:50:03 -0600

    added dependencies

--- client/src/pages/Sell.js
+++ client/src/pages/Sell.js
@@ -22,7 +22,7 @@ const Sell = () => {
 
     const sendImage = (e) => {
         e.preventDefault();
-        console.log(file);
+        console.log(file, "filemane");
         uploadService.sendImages(
             name, description, price, tags, file
         ).then((result) => {
@@ -31,7 +31,7 @@ const Sell = () => {
     };
 
     const onFileChange = (e) => {
-        if(e.target.files && e.target.files.length > 0) {
+        if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
             if (file.type.includes('image')) {
                 const reader = new FileReader();
@@ -40,46 +40,46 @@ const Sell = () => {
                     setPathImages(reader.result)
                 }
                 setFile(file);
-            }else {
+            } else {
                 console.log('There was an error');
             }
         }
     }
 
     return (
-        <div className="sell"> 
+        <div className="sell">
             <div className="sell-form-containter">
-            {Auth.loggedIn() ? (
-                <form>
-                    <h2>Let the world discover your art!</h2>
-                    <div className="field-wrap">
-                        <label>Name </label>
-                        <input type="text" onChange={(e) => setName(e.target.value)} required autocomplete="off"/>
-                    </div>
-                    <div class="field-wrap">
-                        <label>Description (140 characters max)</label>
-                        <textarea maxlength="140" name="description" rows="3" cols="50" onChange={(e) => setDescription(e.target.value)} required></textarea>
-                    </div>
-                    <div class="field-wrap">
-                        <label>Price</label>
-                        <input type="text" onChange={(e) => setPrice(e.target.value)} required autocomplete="off"/>
-                    </div>
-                    <div class="field-wrap">
-                        <label>Tags</label>                        
-                        <input type="tags" onChange={(e) => setTags(e.target.value)} required autocomplete="off"/>
-                    </div>
-                    <div class="field-wrap">
-                        <label>Image</label>
-                        <input type="file" name="fileImg" onChange={onFileChange} required autocomplete="off"/>
-                    </div>
-                    <div className="button-started-container">
-                        <button type="submit" className="btn button-animated" onClick={sendImage}>Publish</button>
-                    </div>
-                </form>
+                {Auth.loggedIn() ? (
+                    <form>
+                        <h2>Let the world discover your art!</h2>
+                        <div className="field-wrap">
+                            <label>Name </label>
+                            <input type="text" onChange={(e) => setName(e.target.value)} required autocomplete="off" />
+                        </div>
+                        <div class="field-wrap">
+                            <label>Description (140 characters max)</label>
+                            <textarea maxlength="140" name="description" rows="3" cols="50" onChange={(e) => setDescription(e.target.value)} required></textarea>
+                        </div>
+                        <div class="field-wrap">
+                            <label>Price</label>
+                            <input type="text" onChange={(e) => setPrice(e.target.value)} required autocomplete="off" />
+                        </div>
+                        <div class="field-wrap">
+                            <label>Tags</label>
+                            <input type="tags" onChange={(e) => setTags(e.target.value)} required autocomplete="off" />
+                        </div>
+                        <div class="field-wrap">
+                            <label>Image</label>
+                            <input type="file" name="fileImg" onChange={onFileChange} required autocomplete="off" />
+                        </div>
+                        <div className="button-started-container">
+                            <button type="submit" className="btn button-animated" onClick={sendImage}>Publish</button>
+                        </div>
+                    </form>
                 ) : (
                     <>
-                    <h3>Sorry! It seems that you are not logged in!</h3>
-                    <p>Please <Link to="/signForm"><strong>sign in</strong></Link> to start discovering...</p>
+                        <h3>Sorry! It seems that you are not logged in!</h3>
+                        <p>Please <Link to="/signForm"><strong>sign in</strong></Link> to start discovering...</p>
                     </>
                 )}
             </div>
